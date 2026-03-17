039f8175ff1258c9c79812b3118f9ca1c2e7f4b4
Tglevel <tglevels@gmail.com>
2026-03-17 17:02:45 +0530

    ad

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (typeof localStorage === "undefined" || localStorage === null) {
 }
 /////////////------------ogbot
 // const telegramtoken = '8199688040:AAHGqr4cECCMb9kd4qXNM5bKAXXrqj8shQk';
-const telegramchat = "-1003727905299";
+// const telegramchat = "-1003727905299";
 /////////////------------pgfbot
 // const telegramtoken = "8390227157:AAFYQ2eWFAJdm9P8me9Nk2voYe00Mn33dSU";
 // const telegramchat = "8559767849";
@@ -48,7 +48,7 @@ const telegramchat = "-1003727905299";
 // const telegramchat = "7781596314";
 /////////////------------Lazy bot
 // const telegramtoken = "8529663033:AAEBTgtqjKdqg3lG89ZMclD8lPTxN7mp3BI"
-// const telegramchat = "8559767849";
+const telegramchat = "8559767849";
 
 let patternSchedulerTimeout = null;
 let isExecuting = false;
@@ -1302,7 +1302,7 @@ const stopPatternScheduler = () => {
 };
 
 // Start the scheduler
-// startPatternScheduler();
+startPatternScheduler();
 // runauth();
 // startlogic(true)
 // authenticate()
